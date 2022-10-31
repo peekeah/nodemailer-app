704c5b265b6ddd43e71ac31bee4b3b491d3aae8e
Pranay Kothari <pranay.kothari109@gmail.com>
2022-10-31 17:36:26 +0530

    added auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+exports.authenticateToken = async(req, res, next) => {
+    // validating token in headers
+    const existToken = req.headers['access-token'];
+    if(!existToken) res.status(401).send({msg: 'Unauthorized: Token not found'});
+
+    // token verification
+    try {
+        req.body.tokenData = jwt.verify(existToken, process.env.JWT_SECRET);
+        next();        
+    } catch (err) {
+        console.log(err);
+        res.status(401).send({msg: 'Unauthorized: Invalid token'});
+    }
+}
